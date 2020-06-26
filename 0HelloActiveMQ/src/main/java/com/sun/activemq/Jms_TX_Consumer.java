3f696ea4b82dcf113755e00587519a86d5816edd
Sunxz007 <977622085@qq.com>
2020-06-26 14:34:58 +0800

    ActiveMQ 签收

--- 0HelloActiveMQ/src/main/java/com/sun/activemq/Jms_TX_Consumer.java
+++ 0HelloActiveMQ/src/main/java/com/sun/activemq/Jms_TX_Consumer.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class Jms_TX_Consumer {
     private static final String ACTIVEMQ_URL = "tcp://localhost:61617";
-    private static final String ACTIVEMQ_QUEUE_NAME = "Queue-TX";
+    private static final String ACTIVEMQ_QUEUE_NAME = "Queue-ACK";
 
     public static void main(String[] args) throws JMSException, IOException {
 
@@ -15,36 +15,23 @@ public class Jms_TX_Consumer {
         Connection connection = activeMQConnectionFactory.createConnection();
         connection.start();
         // 创建session会话，两个参数transacted=事务,acknowledgeMode=确认模式(签收)
-        Session session = connection.createSession(true, Session.CLIENT_ACKNOWLEDGE);
+        Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
         Queue queue = session.createQueue(ACTIVEMQ_QUEUE_NAME);
         MessageConsumer messageConsumer = session.createConsumer(queue);
         messageConsumer.setMessageListener(new MessageListener(){
-            int a = 0;
-
             @Override
             public void onMessage(Message message) {
                 try {
-                    if(null != message && message instanceof TextMessage){
-                        TextMessage textMessage=(TextMessage)message;
+                    if (null != message && message instanceof TextMessage) {
+                        TextMessage textMessage = (TextMessage) message;
                         System.out.println("***消费者接收到的消息:   " + textMessage.getText());
-                        if (a == 0) {
-                            System.out.println("commit");
-                            // 消费者开启了事务就必须手动提交，不然会重复消费消息
-                            session.commit();
-                        } else if (a==2) {
-                            System.out.println("rollback");
-                            session.rollback();
-                        }
+                        //设置为Session.CLIENT_ACKNOWLEDGE后，要调用该方法，标志着该消息已被签收（消费）。
+                        // 如果不调用该方法，该消息的标志还是未消费，下次启动消费者或其他消费者还会收到改消息。
+                        textMessage.acknowledge();
                     }
-                    a++;
                 } catch (JMSException e) {
                     e.printStackTrace();
                     System.out.println("出现异常，消费失败，放弃消费");
-                    try {
-                        session.rollback();
-                    } catch (JMSException ex) {
-                        ex.printStackTrace();
-                    }
                 }
             }
         });
